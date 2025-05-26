175ef0b2dfcbf16ffe4fc1ed72b0f1dfef582fef
lipapipa <153609062+lipapipa@users.noreply.github.com>
2025-05-26 14:28:57 +0500

    Update airflow.py

--- airflow.py
+++ airflow.py
@@ -17,50 +17,42 @@ from datetime import timedelta
 from train_model import train
 
 def download_data():
-    df = pd.read_csv('https://raw.githubusercontent.com/dayekb/Basic_ML_Alg/main/cars_moldova_no_dup.csv', delimiter = ',')
-    df.to_csv("cars.csv", index = False)
-    print("df: ", df.shape)
+    df = pd.read_csv('https://raw.githubusercontent.com/Alexey3250/Start_ML/refs/heads/main/Machine_learning/10_classification/banking.csv', delimiter = ',')
     return df
 
 def clear_data():
-    df = pd.read_csv("cars.csv")
+    df = pd.read_csv('https://raw.githubusercontent.com/Alexey3250/Start_ML/refs/heads/main/Machine_learning/10_classification/banking.csv', delimiter = ',')
+
+    ### Уберем колонку duration
+
+    df = df.drop('duration', axis=1)
+    ### Посмотрим на категориальные колонки
+
+    categorical_columns = df.loc[:,df.dtypes=='object'].columns
+
+    df = df.drop(['loan', 'housing', 'marital'], axis=1)
+    categorical_columns = categorical_columns.drop(['loan', 'housing', 'marital'])
+
+    for col in categorical_columns:
     
-    cat_columns = ['Make', 'Model', 'Style', 'Fuel_type', 'Transmission']
-    num_columns = ['Year', 'Distance', 'Engine_capacity(cm3)', 'Price(euro)']
+        ### К колонкам с маленькой размерностью применим one-hot
+        if df[col].nunique() < 5:
+            one_hot = pd.get_dummies(df[col], prefix=col, drop_first=True).astype(int)
+            df = pd.concat((df.drop(col, axis=1), one_hot), axis=1)
+            
+        ### К остальным - счетчики
+        else:
+            mean_target = df.groupby(col)['y'].mean()
+            df[col] = df[col].map(mean_target)
     
-    question_dist = df[(df.Year <2021) & (df.Distance < 1100)]
-    df = df.drop(question_dist.index)
-    # Анализ и очистка данных
-    # анализ гистограмм
-    question_dist = df[(df.Distance > 1e6)]
-    df = df.drop(question_dist.index)
-    
-    # здравый смысл
-    question_engine = df[df["Engine_capacity(cm3)"] < 200]
-    df = df.drop(question_engine.index)
-    
-    # здравый смысл
-    question_engine = df[df["Engine_capacity(cm3)"] > 5000]
-    df = df.drop(question_engine.index)
-    
-    # здравый смысл
-    question_price = df[(df["Price(euro)"] < 101)]
-    df = df.drop(question_price.index)
-    
-    # анализ гистограмм
-    question_price = df[df["Price(euro)"] > 1e5]
-    df = df.drop(question_price.index)
-    
-    #анализ гистограмм
-    question_year = df[df.Year < 1971]
-    df = df.drop(question_year.index)
+    categorical_columns = df.loc[:,df.dtypes=='object'].columns
     
     df = df.reset_index(drop=True)  
     ordinal = OrdinalEncoder()
-    ordinal.fit(df[cat_columns]);
-    Ordinal_encoded = ordinal.transform(df[cat_columns])
-    df_ordinal = pd.DataFrame(Ordinal_encoded, columns=cat_columns)
-    df[cat_columns] = df_ordinal[cat_columns]
+    ordinal.fit(df[categorical_columns])
+    Ordinal_encoded = ordinal.transform(df[categorical_columns])
+    df_ordinal = pd.DataFrame(Ordinal_encoded, columns=categorical_columns)
+    df[categorical_columns] = df_ordinal[categorical_columns]
     df.to_csv('df_clear.csv')
     return True
 
